cd91e4921d45a4ae80f724bbb8be91114f9349bf
Tomm-debug <tobergallo@gmail.com>
2025-02-17 01:16:13 -0300

    Add files via upload

---
+++ Funcion_iterativa_para_calcular_el_factorial_de_un_numero/Funcion_iterativa.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int factorialIterativo(int n) {
+    int resultado = 1;
+    for (int i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
+int main() {
+    int n;
+    cout << "Ingrese un numero: ";
+    cin >> n;
+
+    cout << "Factorial de " << n << ": " << factorialIterativo(n) << endl;
+
+    int abc ;
+    cin >> abc ;
+    return 0;
+}
---
+++ Funcion_recursiva_para_calcular_la_suma_de_numeros_naturales/Funcion_recursiva.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sumaRecursiva(int n) {
+    if (n == 0) return 0;
+    return n + sumaRecursiva(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Ingrese un numero: ";
+    cin >> n;
+
+    cout << "Suma de los primeros " << n << " numeros naturales: " << sumaRecursiva(n) << endl;
+
+    int abc ;
+    cin >> abc ;
+    return 0;
+}
